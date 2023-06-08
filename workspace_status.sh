41602ae2e9da404a2e08a982f295dfd8f5777753
Daniel P. Purkhús <purkhusid@gmail.com>
2023-06-08 11:17:37 +0000

    wip

--- workspace_status.sh
+++ workspace_status.sh
@@ -5,7 +5,7 @@ set -e
 # Set the correct COMMIT_SHA in case we are running in Github Actions and the workflow was triggered by a pull request
 # If the workflow is triggered by a PR the GITHUB_SHA env is set to the sha of the merge commit so we need to
 # override it so that the correct commit is used in the bazel build and tools that create github checks use the correct
-# git commit to pin it to.
+# git commit to pin it to
 if [ -n "${GITHUB_ACTIONS}" ]; then
     PR_HEAD_SHA="$(cat "${GITHUB_EVENT_PATH}" | jq -r '.pull_request.head.sha')"
     if [ "${PR_HEAD_SHA}" != "null" ]; then
